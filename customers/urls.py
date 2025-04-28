c06ed6889442408779c992d91eb4a424e59fd401
iruy <yuri_matteus@hotmail.com>
2025-04-28 18:22:37 -0300

    jwt implementado

--- customers/urls.py
+++ customers/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import CustomerViewSet
 
+
 router = DefaultRouter()
 router.register(r'customers', CustomerViewSet)
 
--- customers/views.py
+++ customers/views.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets
 from .models import Customer
 from .serializers import CustomerSerializer
+from rest_framework.permissions import IsAuthenticated
 
 class CustomerViewSet(viewsets.ModelViewSet):
     queryset = Customer.objects.all()
