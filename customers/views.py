977ccfd689948180ae12d47e64ef44369a16918c
iruy <yuri_matteus@hotmail.com>
2025-05-14 14:42:27 -0300

    adicionando soma de todos os lucros, custos e custos de serviços

--- customers/views.py
+++ customers/views.py
@@ -1,11 +1,15 @@
-from rest_framework import viewsets, permissions
-from django.core.mail import EmailMessage
-from django.template.loader import render_to_string
+from rest_framework import viewsets, permissions, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from django.utils.timezone import now
+from decimal import Decimal, InvalidOperation
 
 from .models import Customer
 from .serializers import CustomerSerializer
 from .permissions import IsCustomerOwner
 from .utils.pdf_generator import generate_os_pdf
+from django.core.mail import EmailMessage
+from django.template.loader import render_to_string
 
 class CustomerViewSet(viewsets.ModelViewSet):
     queryset = Customer.objects.all()
@@ -19,57 +23,47 @@ class CustomerViewSet(viewsets.ModelViewSet):
         customer = serializer.save(user=self.request.user)
         pdf_buffer = generate_os_pdf(customer)
 
-        subject = f"Ordem de Serviço #{customer.id}"
-        body = render_to_string('customers/emails/os_email.txt', {
-            'customer': customer,
-            'user':     self.request.user,
-        })
-
-        email = EmailMessage(
-            subject=subject,
-            body=body,
-            to=[customer.email, self.request.user.email],
-        )
-        email.attach(f'OS_{customer.id}.pdf', pdf_buffer.read(), 'application/pdf')
-        email.send(fail_silently=False)
-
     def perform_update(self, serializer):
         customer = serializer.save()
         pdf_buffer = generate_os_pdf(customer)
 
-        subject = f"Ordem de Serviço (Atualizada) #{customer.id}"
-        body = render_to_string('customers/emails/os_email.txt', {
-            'customer': customer,
-            'user':     self.request.user,
-        })
+    def perform_destroy(self, instance):
+        instance.delete()
 
-        email = EmailMessage(
-            subject=subject,
-            body=body,
-            to=[customer.email, self.request.user.email],
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response(
+            {'detail': 'OS excluída com sucesso.'},
+            status=status.HTTP_204_NO_CONTENT
         )
-        email.attach(f'OS_{customer.id}.pdf', pdf_buffer.read(), 'application/pdf')
-        email.send(fail_silently=False)
 
-        def perform_destroy(self, instance):
-                user = self.request.user
-                subject = f"Ordem de Serviço #{instance.id} deletada"
-                body = render_to_string('customers/emails/os_deleted.txt', {
-                    'customer': instance,
-                    'user':     user,
-                })
-                email = EmailMessage(
-                    subject=subject,
-                    body=body,
-                    to=[instance.email, user.email],
-                )
-                email.send(fail_silently=True)
+    @action(detail=False, methods=['get'], url_path='monthly-summary')
+    def monthly_summary(self, request):
+      
+        today = now()
+        qs = self.get_queryset().filter(
+            created_at__year=today.year,
+            created_at__month=today.month
+        )
 
-                instance.delete()
+        total_cost = Decimal('0.00')
+        total_service = Decimal('0.00')
 
-        def destroy(self, request, *args, **kwargs):
-           
-            instance = self.get_object()
-            self.perform_destroy(instance)
-            return Response({'detail': 'OS excluída com sucesso.'},
-                            status=status.HTTP_204_NO_CONTENT)
+        for obj in qs:
+            try:
+                total_cost += Decimal(obj.cost_price)
+            except (InvalidOperation, TypeError):
+                pass
+            try:
+                total_service += Decimal(obj.service_price)
+            except (InvalidOperation, TypeError):
+                pass
+
+        total_profit = total_service - total_cost
+
+        return Response({
+            'total_cost_price':    f'{total_cost:.2f}',
+            'total_service_price': f'{total_service:.2f}',
+            'total_profit':        f'{total_profit:.2f}',
+        }, status=status.HTTP_200_OK)
